96dba08e6db65cfe86d7da7a4fc817152d345314
abishop9 <abishop9@webdmc.delmar.edu>
2024-11-19 10:36:31 -0600

    Updated version of the project

--- Project_AB.cpp
+++ Project_AB.cpp
@@ -2,20 +2,32 @@
 
 #include <iostream>
 #include <iomanip>
+#include <fstream>
+#include <string>
 #include <cstdlib>
 using namespace std;
 
 void userSignIn();
 void userSignUp();
-void guestAccess();
+void Room_Type();
+int Receipt(int, int, int, int);
+bool getOccupied();
+void pickRoom(int);
+
+const int MAX_FLOORS = 3;
+const int MAX_ROOMS = 10;
+int hotel[MAX_FLOORS][MAX_ROOMS];
+
+bool occupied[MAX_ROOMS];
+
+const int A_PRICE = 80;
+const int B_PRICE = 150;
+const int C_PRICE = 175;
+
+
 
 int main()
 {
-    const int MAX_FLOORS = 20;
-    int floor1[MAX_FLOORS];
-    int floor2[MAX_FLOORS];
-    int floor3[MAX_FLOORS];
-
     char choice;
     char again;
 
@@ -48,17 +60,105 @@ int main()
     return 0;
 }
 
-void userSignIn()
+
+void signin()
 {
 
+	display "Please enter your username"
+	input username
+	display "Please enter your password"
+	input password
+
+	do
+    {
+	Display "Input checkin date"
+	input Checkin_Date
+    } while(checkin_Date > 1 AND checkin_Date < 31);
+
+    do
+    {
+        Display "Input Checkout date"
+        input Checkout_Date
+    } while(checkout_Date > 2 AND checkout_Date < 31);
 }
 
-void userSignUp()
+
+void signup()
 {
 
+	display "Welcome to Signup for Hotel Overlook"
+	display "                                                                      "
+	display "                                                                      "
+	display "**********************************************************************"
+	display "Please enter your username"
+	input username
+	display "Please enter your password"
+	input password
 }
 
-void guestAccess()
-{
 
+void Room_Type()
+{
+    int price;
+    bool occupied;
+    string room_type;
+
+    cout << "Choose room type:" << endl;
+    cout << "1) Single" << endl "2) Double" << endl << "3) Suite" << endl;
+    cin >> room_type;
+
+    switch(room_type)
+        case 1: Single...
+        price = A_PRICE;     // Price, Random picked location based on type
+        randomNumber();
+        break;
+
+        case 2: Double...
+        price = B_PRICE;     // Price, Random picked location based on type
+        occupied = randomNumber();
+        break;
+
+        case 3: Suite...
+        price = C_PRICE;     // Price, Random picked location based on type
+        occupied = randomNumber();
+        break;
+	cout << "There are " << occupied << " rooms available for your dates" << endl;
+
+	pickRoom(occupied);
+}
+
+
+int Receipt(price,total,checkin_Date,checkout_Date)
+{
+    Display "--- Receipt ---"
+    Display "Room type price: $" + price
+    Display "Total nights: " + (checkout_Date - checkin_Date)
+    Display "Total amount due: $" + total
+}
+
+
+bool getOccupied()
+{
+    srand(time(0));
+    bool occupied;
+
+    for (int )
+    {
+        occupied = rand() % 2;
+
+    }
+}
+
+
+void pickRoom(occupied)
+{
+    int choice;
+    for (int row = 0; row <= MAX_ROOMS; row++)
+    {
+        cout << "Rooms: " << hotel[][] << " ";
+        for (int column = 0; column <= MAX_ROOMS; column++)
+        {
+
+        }
+    }
 }
